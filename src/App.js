b6aff6e662a0c8aac6c9548d6bd34268abc5dd3e
li-lauren <71899440+li-lauren@users.noreply.github.com>
2021-03-30 15:15:18 -0600

    Create app skeleton (#1)
    * Add google fonts
    
    * Add index.css and import normalize.css
    
    * Add react router and styled components
    
    * Add router with routes / and /search
    
    * Add global styling
    
    * Factor out GlobalStyle
    
    * Add local fonts
    
    * Add GlobalStyle
    
    * Add 404 route
    
    * Add styled-normalize
    
    * Fix linting errors and add theme wrapper
    
    * Add theme object

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Switch>
+          <Route exact path="/">Home</Route>
+          <Route path="/search">Search</Route>
+          <Route>404 - Not Found</Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
