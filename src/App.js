47aad90c027a991ed7a0dd7fb235e02df179039f
li-lauren <laurenlidev@gmail.com>
2021-03-29 20:44:19 -0600

    Add router with routes / and /search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <div>
+        App Placeholder
+        <Switch>
+          <Route exact path="/">Home</Route>
+          <Route path="/search">Search</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
