61f8a38b5c77598a915366124e8a8ac17a76c8f9
li-lauren <laurenlidev@gmail.com>
2021-04-06 13:23:06 -0600

    Move Router to index for testing purposes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './app';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, document.getElementById('root'));
