b86b08fbecc3ef2a18affef25898f22d5a3dfd8f
li-lauren <laurenlidev@gmail.com>
2021-04-07 13:17:11 -0600

    Add tests for nav bar sectional links

--- src/__tests__/Header.js
+++ src/__tests__/Header.js
@@ -40,4 +40,18 @@ test('navigates to search page when search link is clicked', () => {
 
   expect(screen.getByText(/search page/i)).toBeInTheDocument();
   expect(history.location.pathname).toEqual('/search/javascript');
-})
+});
+
+test.each`
+  link | hash
+  ${'About'} | ${'#about'}
+  ${'How it works'} | ${'#how-it-works'}
+  `('navigates to "$link" section when "$link" link is clicked', ({ link, hash}) => {
+    const { history } = setup('/search/javascript');
+
+    const hashLink = screen.getByRole('link', { name: link });
+    userEvent.click(hashLink);
+
+    expect(screen.getByText(/home page/i)).toBeInTheDocument();
+    expect(history.location.hash).toEqual(hash);
+  });
